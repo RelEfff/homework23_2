77d9b87248253e69f35009b91d11a1e31b27590f
Ivan Okunkov <relativistic_effects@mail.ru>
2024-09-16 19:47:34 +0300

    added 1.0

--- config/settings.py
+++ config/settings.py
@@ -15,6 +15,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'catalog',
 ]
 
 MIDDLEWARE = [
@@ -49,8 +51,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'homework20_1',
+        'USER': 'postgres',
+        'PASSWORD': '88ivan33',
+        'HOST': '127.0.0.1',
+        'PORT': "5432"
     }
 }
 
